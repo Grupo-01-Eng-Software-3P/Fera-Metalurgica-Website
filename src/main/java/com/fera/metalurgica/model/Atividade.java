fb5fe63b9d6ea5bedca161070c395057e4dd3213
EduuUuW <eduardowillemann1@gmail.com>
2026-05-14 21:17:01 -0300

    feat: Banco de dados conectado a pagina agenda, funcionando

--- src/main/java/com/fera/metalurgica/model/Atividade.java
+++ src/main/java/com/fera/metalurgica/model/Atividade.java
@@ -1,6 +1,7 @@
 package com.fera.metalurgica.model;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "atividade")
@@ -13,29 +14,21 @@ public class Atividade {
     private String titulo;
     private String descricao;
     private String evento;
-    private String data;
+    private LocalDate data;
     private String horario;
 
     public Atividade() {}
-
-    public Atividade(String titulo, String descricao, String evento, String data, String horario) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.evento = evento;
-        this.data = data;
-        this.horario = horario;
-    }
 
     public Long getId() { return id; }
     public String getTitulo() { return titulo; }
     public String getDescricao() { return descricao; }
     public String getEvento() { return evento; }
-    public String getData() { return data; }
+    public LocalDate getData() { return data; }
     public String getHorario() { return horario; }
 
     public void setTitulo(String titulo) { this.titulo = titulo; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
     public void setEvento(String evento) { this.evento = evento; }
-    public void setData(String data) { this.data = data; }
+    public void setData(LocalDate data) { this.data = data; }
     public void setHorario(String horario) { this.horario = horario; }
 }
